7c88b76929d3f52056bba4b0134f94932ca14824
sanjeewk <sanjeew.kanagaraj@gmail.com>
2023-02-04 22:42:42 +0800

    migrate to postgresql database

--- content_aggregator/settings.py
+++ content_aggregator/settings.py
@@ -79,10 +79,18 @@ WSGI_APPLICATION = 'content_aggregator.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    'NAME': 'project_database',
+    'USER': 'admin',
+    'PASSWORD': 'Admin123',
+    'HOST': 'localhost',
+    'PORT': ''  
+} 
 }
 
 
